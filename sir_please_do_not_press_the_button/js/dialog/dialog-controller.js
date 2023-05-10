c45514d4c6e2666e0d65af54fa6c6b0711f7c5ce
Robin <>
2023-05-10 21:20:38 +0200

    Add more functionality for the dialog controller and add in dialog events

--- sir_please_do_not_press_the_button/js/dialog/dialog-controller.js
+++ sir_please_do_not_press_the_button/js/dialog/dialog-controller.js
@@ -1,5 +1,5 @@
-import { Component, Property } from '@wonderlandengine/api';
-import { DialogManager } from './dialog-manager'
+import {Component, Emitter, Property} from '@wonderlandengine/api';
+import {DialogManager} from './dialog-manager'
 
 /**
  * dialog-controller
@@ -15,35 +15,68 @@ export class DialogController extends Component {
         dialog: Property.string(""),
         charDelay: Property.float(0.025),
         blankDelay: Property.float(0.5),
+        audioSource: Property.object(),
+        dialogPrefix: Property.string('> '),
     };
 
     init() {
         this.reset();
-        this.currentText = '> ';
+
         this.responseTexts = new Array();
         this.responseTexts.push(this.responseOneText.getComponent('text'));
         this.responseTexts.push(this.responseTwoText.getComponent('text'));
-    }
 
-    reset() {
-        this.currentText = '> ';
-        this.currentState = "";
-        this.currentStateJSON = null;
-        this.textReadPos = 0;
-        this.timer = 0.0;
+        this.onHideResponses = new Emitter();
+        this.onSetupResponses = new Emitter();
     }
 
     play() {
-        if (this.currentStateJSON) return;
+        if(this.currentStateJSON) return;
         this.reset();
         this.playingDialog = this.dialogManager.getComponent(DialogManager).getDialog(this.dialog);
         this.currentState = "starting";
     }
 
-    update(dt) {
-        if (this.currentState == "") return;
+    stop() {
+        this.reset();
+    }
 
-        if (this.currentState == "starting") {
+    pause() {
+        if(this.paused) return;
+        this.hideResponses();
+        this.paused = true;
+        this.textReadPos = 0;
+        this.currentText = this.dialogPrefix;
+        this.text.getComponent('text').text = this.currentText;
+    }
+
+    resume() {
+        if(!this.paused) return;
+        this.paused = false;
+    }
+
+    isWaitingForResponse() {
+        if(!this.currentStateJSON) return false;
+        var desiredText = this.currentStateJSON["text"];
+        return this.textReadPos >= desiredText.length;
+    }
+
+    reset() {
+        this.hideResponses();
+        this.paused = false;
+        this.currentText = this.dialogPrefix;
+        this.currentState = "";
+        this.currentStateJSON = null;
+        this.textReadPos = 0;
+        this.timer = 0.0;
+
+        this.text.getComponent('text').text = this.currentText;
+    }
+
+    update(dt) {
+        if(this.currentState == "" || this.paused) return;
+
+        if(this.currentState == "starting") {
             this.reset();
             this.currentStateJSON = this.playingDialog["entry"];
             this.currentState = "entry";
@@ -51,28 +84,80 @@ export class DialogController extends Component {
         }
 
         var desiredText = this.currentStateJSON["text"];
-        if (this.textReadPos < desiredText.length) {
+        if(this.textReadPos < desiredText.length) {
             const char = desiredText[this.textReadPos];
-            // TODO: Handle events
+            // Hello [s:name] World!
+            // s: sound
+            // a: animation
+            // e: event
+            if(char == '[') {
+                var firstBracketIndex = this.textReadPos;
+                var secondBracketIndex = -1;
+                while(true) {
+                    ++this.textReadPos;
+                    if(this.textReadPos >= desiredText.length) {
+                        console.error("Missing ']' in dialog sub event");
+                        return;
+                    }
+                    var nextChar = desiredText[this.textReadPos];
+                    if(nextChar == ']') {
+                        secondBracketIndex = this.textReadPos;
+                        ++this.textReadPos;
+                        break;
+                    }
+                }
+
+                const eventData = desiredText.substring(firstBracketIndex + 1, secondBracketIndex);
+                const tokens = eventData.split(':', 2);
+                if(tokens.length < 2) {
+                    console.error('Expected type and name for dialog sub event');
+                    return;
+                }
+
+                const type = tokens[0];
+                const name = tokens[1];
+
+                switch(type) {
+                    case 's': {
+                        // Sound
+                        this.dialogManager.getComponent(DialogManager).triggerSound(name, this.audioSource);
+                        break;
+                    }
+
+                    case 'a': {
+                        // Animation
+                        this.dialogManager.getComponent(DialogManager).triggerAnimation(name);
+                        break;
+                    }
+
+                    case 'e': {
+                        // Custom Event
+                        this.dialogManager.getComponent(DialogManager).dispatchEvent(name);
+                        break;
+                    }
+                }
+                // No need to continue processing the next character, next frame will do that
+                return;
+            }
 
             const ignore = char == '_';
             const delay = char == '_' ? this.blankDelay : this.charDelay;
 
             this.timer += dt;
-            if (this.timer < delay) return;
+            if(this.timer < delay) return;
             this.timer -= delay;
 
-            if (!ignore) this.currentText += char;
+            if(!ignore) this.currentText += char;
             ++this.textReadPos;
 
-            if (this.textReadPos >= desiredText.length) {
+            if(this.textReadPos >= desiredText.length) {
                 this.setupResponses();
             }
         } else {
             var autoAdvanceTime = this.currentStateJSON["autoAdvanceAfter"];
-            if (autoAdvanceTime) {
+            if(autoAdvanceTime) {
                 this.timer += dt;
-                if (this.timer < autoAdvanceTime) return;
+                if(this.timer < autoAdvanceTime) return;
                 this.timer -= autoAdvanceTime;
                 this.advance(-1);
             }
@@ -82,14 +167,21 @@ export class DialogController extends Component {
     }
 
     advance(choiceIndex) {
-        if (!this.currentStateJSON) return;
+        if(this.paused) {
+            console.warn("Cannot advance a paused dialog!");
+            return;
+        }
+        if(!this.currentStateJSON) return;
+
+        // Reset responses
+        this.hideResponses();
 
         var responses = this.currentStateJSON["responses"];
 
         var jump = responses ? null : this.currentStateJSON["jump"];
-        if (!jump && responses && choiceIndex != -1) {
+        if(!jump && responses && choiceIndex != -1) {
             var response = responses[choiceIndex];
-            if (!response) {
+            if(!response) {
                 console.log("Cannot advance with invalid response!");
                 return;
             }
@@ -97,19 +189,19 @@ export class DialogController extends Component {
         }
 
         // Cannot advance without a response
-        if (!jump && responses) {
+        if(!jump && responses) {
             console.log("Cannot advance current dialog without response!");
             return;
         }
 
-        if (!jump && jump != "") {
+        if(!jump && jump != "") {
             var keys = Object.keys(this.playingDialog);
             var index = keys.indexOf(this.currentState);
             this.reset();
 
-            if (index + 1 >= keys.length) {
+            if(index + 1 >= keys.length) {
                 // Done!
-                this.dialogManager.getComponent(DialogManager).end(this);
+                this.dialogManager.getComponent(DialogManager).onEnd(this);
                 return;
             }
             this.currentState = keys[index + 1];
@@ -124,18 +216,28 @@ export class DialogController extends Component {
         this.handleEvent();
     }
 
+    hideResponses() {
+        this.onHideResponses.notify();
+        for(var i = 0; i < this.responseTexts.length; ++i) {
+            this.responseTexts[i].active = false;
+            this.responseTexts[i].text = "";
+        }
+    }
+
     setupResponses() {
+        this.onSetupResponses.notify();
         var responses = this.currentStateJSON["responses"];
-        if (!responses) return;
-        for (var i = 0; i < responses.length; ++i) {
+        if(!responses) return;
+        for(var i = 0; i < responses.length; ++i) {
             var response = responses[i]["text"];
+            this.responseTexts[i].active = true;
             this.responseTexts[i].text = response;
         }
     }
 
     handleEvent() {
         const event = this.currentStateJSON["event"];
-        if (!event) return;
+        if(!event) return;
         this.dialogManager.getComponent(DialogManager).dispatchEvent(event);
     }
 }
--- sir_please_do_not_press_the_button/js/dialog/dialog-manager.js
+++ sir_please_do_not_press_the_button/js/dialog/dialog-manager.js
@@ -1,4 +1,4 @@
-import { Component, Emitter, Property } from '@wonderlandengine/api';
+import {Component, Emitter, Property} from '@wonderlandengine/api';
 
 /**
  * dialog-manager
@@ -17,27 +17,87 @@ export class DialogManager extends Component {
         this.events = new Map();
     }
 
+    /**
+     * Get a dialog json object by name
+     * @param name Name of the dialog to get
+     */
     getDialog(name) {
         return this.dialogs[name];
     }
 
+    /**
+     * Start a dialog on a dialog controller
+     * @param dialogController Controller to play
+     */
     play(dialogController) {
-        if (this.playingDialog) return;
-
+        if(this.playingDialog) return;
         this.playingDialog = dialogController;
         dialogController.play();
     }
 
-    end(dialog) {
-        if (this.playingDialog != dialog) return;
+    /**
+     * Triggered when a dialog comes to an end
+     * Internal function, pls no touch
+     * @param dialogController Controller that ended
+     */
+    onEnd(dialogController) {
+        if(this.playingDialog != dialogController) return;
         this.playingDialog = null;
     }
 
+    /**
+     * Stop the current dialog which resets it
+     */
+    stop() {
+        if(!this.playingDialog) return;
+        this.playingDialog.stop();
+        this.playingDialog = null;
+    }
+
+    /**
+     * Pause the current dialog
+     * This also resets the text animator
+     */
+    pause() {
+        if(!this.playingDialog) return;
+        this.playingDialog.pause();
+    }
+
+    /**
+     * Resume the current dialog
+     */
+    resume() {
+        if(!this.playingDialog) return;
+        this.playingDialog.resume();
+    }
+
+    /**
+     * Advance the current dialog with a response
+     * @param choiceIndex Index of the response to advance with, provide -1 if there are no responses
+     */
     advance(choiceIndex) {
-        if (!this.playingDialog) return;
+        if(!this.playingDialog) return;
         this.playingDialog.advance(choiceIndex);
     }
 
+    /**
+     * Wether the dialog is waiting for a response or timer
+     */
+    isWaitingForResponse() {
+        if(!this.playingDialog) return false;
+        return this.playingDialog.isWaitingForResponse();
+    }
+
+    /**
+     * Wether a dialog is currently playing
+     */
+    isPlaying() {
+        return this.playingDialog != undefined;
+    }
+
+    /**
+     * Add a listener for a dialog event
+     */
     addEventListener(name, callback) {
         if (!this.events.has(name)) {
             this.events.set(name, new Emitter());
@@ -45,9 +105,21 @@ export class DialogManager extends Component {
         this.events.get(name).add(callback);
     }
 
+    /**
+     * Dispatch a dialog event
+     * @param name Name of the event to dispatch
+     */
     dispatchEvent(name) {
         if (!this.events.has(name)) return;
         const emitter = this.events.get(name);
         emitter.notify();
+    }
+
+    triggerSound(name, source) {
+        // TODO
+    }
+
+    triggerAnimation(name) {
+        // TODO
     }
 }
