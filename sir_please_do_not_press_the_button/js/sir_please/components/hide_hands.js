3f396ef1be7bf0e192be7128de6d0098f9005511
Pipo <eliaducceschi@hotmail.it>
2024-08-23 15:28:24 +0200

    Disable hands on explode

--- sir_please_do_not_press_the_button/js/sir_please/components/hide_hands.js
+++ sir_please_do_not_press_the_button/js/sir_please/components/hide_hands.js
@@ -1,5 +1,5 @@
 import { Component, Property } from "@wonderlandengine/api";
-import { XRUtils } from "../../pp";
+import { Globals, Handedness, XRUtils } from "../../pp";
 
 export class HideHandsComponent extends Component {
     static TypeName = "hide-hands";
@@ -10,13 +10,23 @@ export class HideHandsComponent extends Component {
 
     start() {
         this._myShowHands = false;
+
+        this._myHandPoseRight = Globals.getHandPose(Handedness.RIGHT);
+        this._myHandPoseLeft = Globals.getHandPose(Handedness.LEFT);
     }
 
     update(dt) {
-        if (XRUtils.isSessionActive()) {
-            if (this._myShowHands) {
-                this._myLeftHand.pp_setActive(true);
+        if (XRUtils.isSessionActive() && this._myShowHands) {
+            if (this._myHandPoseRight.isValid()) {
                 this._myRightHand.pp_setActive(true);
+            } else {
+                this._myRightHand.pp_setActive(false);
+            }
+
+            if (this._myHandPoseLeft.isValid()) {
+                this._myLeftHand.pp_setActive(true);
+            } else {
+                this._myLeftHand.pp_setActive(false);
             }
         } else {
             this._myLeftHand.pp_setActive(false);
