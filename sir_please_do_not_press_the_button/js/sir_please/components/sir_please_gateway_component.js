96d6e61d05ca933531dd123c55b717f54bd3d036
Pipo <eliaducceschi@hotmail.it>
2024-08-25 16:55:31 +0200

    Add game version

--- sir_please_do_not_press_the_button/js/sir_please/components/sir_please_gateway_component.js
+++ sir_please_do_not_press_the_button/js/sir_please/components/sir_please_gateway_component.js
@@ -71,6 +71,10 @@ export class SirPleaseGatewayComponent extends Component {
         if (this._myStartCounter > 0) {
             this._myStartCounter--;
             if (this._myStartCounter == 0) {
+
+                let currentVersion = "0.9.0";
+                console.log("Game Version:", currentVersion);
+
                 this._start();
             }
         } else {
