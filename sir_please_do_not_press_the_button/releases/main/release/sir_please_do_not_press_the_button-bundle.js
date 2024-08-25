2a4ab6cf617faaad15b3670057ed222b5f66ef4f
Pipo <eliaducceschi@hotmail.it>
2024-08-25 17:53:37 +0200

    Update releases

--- sir_please_do_not_press_the_button/releases/main/release/sir_please_do_not_press_the_button-bundle.js
+++ sir_please_do_not_press_the_button/releases/main/release/sir_please_do_not_press_the_button-bundle.js
@@ -57450,6 +57450,136 @@ __publicField(HideHandsComponent, "Properties", {
   _myRightHand: Property.object()
 });
 
+// js/sir_please/components/show_xr_buttons_component.js
+var ShowXRButtonsComponent = class extends Component {
+  init() {
+    this._myShowVRButton = true;
+    this._myVRButtonBehaviorWhenNotAvailable = 2;
+    this._myShowARButton = false;
+    this._myARButtonBehaviorWhenNotAvailable = 2;
+    this._myFirstUpdate = false;
+    this._myVRButtonVisibilityUpdated = false;
+    this._myVRButtonUsabilityUpdated = false;
+    this._myVRButtonDisabledOpacityUpdated = false;
+    this._myARButtonVisibilityUpdated = false;
+    this._myARButtonUsabilityUpdated = false;
+    this._myARButtonDisabledOpacityUpdated = false;
+    this._myXRButtonsContainer = document.getElementById("xr-buttons-container");
+    this._myVRButton = document.getElementById("vr-button");
+    this._myARButton = document.getElementById("ar-button");
+  }
+  start() {
+    XRUtils.registerSessionStartEndEventListeners(this, this._onXRSessionStart.bind(this), this._onXRSessionEnd.bind(this), true);
+  }
+  update(dt) {
+    if (!this._myFirstUpdate) {
+      this._myFirstUpdate = true;
+      if (this._myXRButtonsContainer != null) {
+        if (this._myShowVRButton || this._myShowARButton) {
+          this._myXRButtonsContainer.style.setProperty("display", "flex");
+        } else {
+          this._myXRButtonsContainer.style.setProperty("display", "none");
+        }
+      }
+      if (this._myVRButton != null) {
+        if (this._myShowVRButton) {
+          this._myVRButton.style.setProperty("display", "block");
+        } else {
+          this._myVRButton.style.setProperty("display", "none");
+        }
+      }
+      if (this._myARButton != null) {
+        if (this._myShowARButton) {
+          this._myARButton.style.setProperty("display", "block");
+        } else {
+          this._myARButton.style.setProperty("display", "none");
+        }
+      }
+    } else {
+      this._updateXRButtons(dt);
+    }
+  }
+  _updateXRButtons(dt) {
+    if (this._myShowVRButton) {
+      if (!this._myVRButtonUsabilityUpdated) {
+        if (this._myVRButton != null) {
+          if (!this._myVRButtonVisibilityUpdated) {
+            this._myVRButton.style.setProperty("transform", "scale(1)");
+            this._myVRButtonVisibilityUpdated = true;
+          }
+          if (!this._myVRButtonUsabilityUpdated) {
+            if (XRUtils.isVRSupported()) {
+              this._myVRButton.style.setProperty("opacity", "1");
+              this._myVRButton.style.setProperty("pointer-events", "all");
+              this._myVRButtonUsabilityUpdated = true;
+            } else if (!this._myVRButtonDisabledOpacityUpdated) {
+              switch (this._myVRButtonBehaviorWhenNotAvailable) {
+                case 0:
+                  this._myVRButton.style.setProperty("opacity", "1");
+                  this._myVRButton.style.setProperty("pointer-events", "all");
+                  break;
+                case 1:
+                  this._myVRButton.style.setProperty("opacity", "0.5");
+                  break;
+                case 2:
+                  this._myVRButton.style.setProperty("display", "none");
+                  break;
+              }
+              this._myVRButtonDisabledOpacityUpdated = true;
+            }
+          }
+        } else {
+          this._myVRButtonUsabilityUpdated = true;
+        }
+      }
+    }
+    if (this._myShowARButton) {
+      if (!this._myARButtonUsabilityUpdated) {
+        if (this._myARButton != null) {
+          if (!this._myARButtonVisibilityUpdated) {
+            this._myARButton.style.setProperty("transform", "scale(1)");
+            this._myARButtonVisibilityUpdated = true;
+          }
+          if (!this._myARButtonUsabilityUpdated) {
+            if (XRUtils.isARSupported()) {
+              this._myARButton.style.setProperty("opacity", "1");
+              this._myARButton.style.setProperty("pointer-events", "all");
+              this._myARButtonUsabilityUpdated = true;
+            } else if (!this._myARButtonDisabledOpacityUpdated) {
+              switch (this._myARButtonBehaviorWhenNotAvailable) {
+                case 0:
+                  this._myARButton.style.setProperty("opacity", "1");
+                  this._myARButton.style.setProperty("pointer-events", "all");
+                  break;
+                case 1:
+                  this._myARButton.style.setProperty("opacity", "0.5");
+                  break;
+                case 2:
+                  this._myARButton.style.setProperty("display", "none");
+                  break;
+              }
+              this._myARButtonDisabledOpacityUpdated = true;
+            }
+          }
+        } else {
+          this._myARButtonUsabilityUpdated = true;
+        }
+      }
+    }
+  }
+  _onXRSessionStart() {
+    if (this._myXRButtonsContainer != null) {
+      this._myXRButtonsContainer.style.setProperty("display", "none");
+    }
+  }
+  _onXRSessionEnd() {
+    if (this._myXRButtonsContainer != null && (this._myShowVRButton || this._myShowARButton)) {
+      this._myXRButtonsContainer.style.setProperty("display", "flex");
+    }
+  }
+};
+__publicField(ShowXRButtonsComponent, "TypeName", "pp-show-xr-buttons");
+
 // js/sir_please/components/sir_dialog_button_component.js
 var SirDialogButtonComponent = class extends Component {
   start() {
@@ -58922,6 +59052,7 @@ engine2.registerComponent(ExplodeButtonComponent);
 engine2.registerComponent(FadeViewInOutComponent);
 engine2.registerComponent(HideHandsComponent);
 engine2.registerComponent(SetHandednessComponent);
+engine2.registerComponent(ShowXRButtonsComponent);
 engine2.registerComponent(SirDialogButtonComponent);
 engine2.registerComponent(SirDialogComponent);
 engine2.registerComponent(SirPleaseGatewayComponent);
