3b43a406008aaa9d7d0bfa502bb4fef8e5d9fd36
Pipo <eliaducceschi@hotmail.it>
2024-08-30 16:50:20 +0200

    Sir fly away

--- sir_please/js/sir_please/states/win_state.js
+++ sir_please/js/sir_please/states/win_state.js
@@ -1,4 +1,4 @@
-import { FSM, TimerState, vec3_create } from "../../pp";
+import { FSM, MathUtils, Timer, TimerState, vec3_create } from "../../pp";
 import { GameGlobals } from "../game_globals";
 
 export class WinState {
@@ -29,6 +29,15 @@ export class WinState {
 
         this._myFSM.init("init");
         this._myFSM.perform("start");
+
+        this._mySpawnParticlesTimer = new Timer(0);
+        this._myAccelerationTimer = new Timer(10);
+        this._myMaxParticleTimer = new Timer(10);
+
+        this._myCurrentAcceleration = 0;
+
+        this._myCurrentSpeed = 0;
+        this._myTakeOffSpeed = 0.075;
     }
 
     start(fsm) {
@@ -47,6 +56,12 @@ export class WinState {
 
     _startWin() {
         GameGlobals.myButtonHand.stopButtonHand();
+
+        this._mySpawnParticlesTimer.start(0);
+        this._myAccelerationTimer.start();
+        this._myMaxParticleTimer.start();
+        this._myCurrentSpeed = 0;
+        this._myCurrentAcceleration = 0;
     }
 
     _stopWin() {
@@ -54,9 +69,34 @@ export class WinState {
     }
 
     _flyUpdate(dt, fsm) {
-        GameGlobals.myButtonHand.object.pp_translate(vec3_create(0, 0.5 * dt, 0));
-        this._mySirBody.pp_translate(vec3_create(0, 0.5 * dt, 0));
-        this._mySirExtras.pp_translate(vec3_create(0, 0.5 * dt, 0));
+        if (this._mySirBody.pp_getPosition()[1] < 2000) {
+            if (this._myAccelerationTimer.getPercentage() < 1) {
+                this._myCurrentAcceleration = MathUtils.interpolate(0.001, 2, this._myAccelerationTimer.getPercentage(), easeInExpo);
+            }
+
+            this._myAccelerationTimer.update(dt);
+            this._myMaxParticleTimer.update(dt);
+
+            if (this._myAccelerationTimer.getPercentage() == 1) {
+                this._myCurrentAcceleration += 1 * dt;
+            }
+
+            this._myCurrentSpeed += this._myCurrentAcceleration * dt;
+            this._myCurrentSpeed = Math.min(this._myCurrentSpeed, 500);
+
+            GameGlobals.myButtonHand.object.pp_translate(vec3_create(0, this._myCurrentSpeed * dt, 0));
+            this._mySirBody.pp_translate(vec3_create(0, this._myCurrentSpeed * dt, 0));
+            this._mySirExtras.pp_translate(vec3_create(0, this._myCurrentSpeed * dt, 0));
+
+            this._mySpawnParticlesTimer.update(dt);
+            if (this._mySpawnParticlesTimer.isDone()) {
+                GameGlobals.myHandParticlesSpawner._myScaleMultiplier = 0.010 * Math.pp_mapToRange(this._myMaxParticleTimer.getPercentage(), 0, 1, 0.75, 1.5);
+                GameGlobals.myHandParticlesSpawner._myVerticalSpeedMultiplier = -1 * Math.pp_mapToRange(this._myMaxParticleTimer.getPercentage(), 0, 1, 0.5, 1.25);
+                GameGlobals.myHandParticlesSpawner.spawn(GameGlobals.myButtonHand.object.pp_getPosition());
+
+                this._mySpawnParticlesTimer.start(MathUtils.interpolate(1, 0.01, this._myMaxParticleTimer.getPercentage(), easeTimer));
+            }
+        }
     }
 
     _waitDialogHiddenUpdate(dt, fsm) {
@@ -64,4 +104,12 @@ export class WinState {
             this._myFSM.perform("end");
         }
     }
+}
+
+function easeInExpo(x) {
+    return x === 0 ? 0 : Math.pow(3.5, 10 * x - 10);
+}
+
+function easeTimer(x) {
+    return 1 - (1 - x) * (1 - x);
 }
